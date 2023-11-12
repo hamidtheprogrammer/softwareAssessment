a91236bcb3ffd447ec2724f963da976028798034
HAMID <owoyemiameed@gmaiil.com>
2023-11-12 21:32:45 +0000

    PDF issues

--- Product.cs
+++ Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,6 +37,27 @@ namespace soft
 
             login.messagePrompt("Product added successfully");
 
+        }
+
+        public Product viewProduct(Queries query,int index)
+        {
+            DataSet getproduct = dbconnection.getDataSet(query.getSpecificProduct(index));
+            DataTable getProductTable = getproduct.Tables[0];
+            if( getProductTable.Rows.Count == 1 ) 
+            {
+                int Id = Convert.ToInt16(getProductTable.Rows[0]["Id"]);
+                string productName = getProductTable.Rows[0]["Name"].ToString();
+                string productDescription = getProductTable.Rows[0]["Description"].ToString();
+                string productTypeOfSoftware = getProductTable.Rows[0]["Type_Of_Software"].ToString();
+                string productBusinessArea = getProductTable.Rows[0]["Business_Area"].ToString();
+                byte[] productPDF = Convert.ToByte(getProductTable.Rows[0]["PDF"]);              
+                string productURL = getProductTable.Rows[0]["Link"].ToString();
+
+                Product product = new Product(Id, productName, productDescription, productTypeOfSoftware, productBusinessArea, productPDF, productURL );
+
+                return product;
+                
+            }
         }
 
     }
